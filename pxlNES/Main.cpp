149ae724130e0860faef580def133e40c22ac2f6
Richman Stewart <flynnspixel@gmail.com>
2015-05-03 03:23:47 +1000

    Added enum for all instructions

--- pxlNES/Main.cpp
+++ pxlNES/Main.cpp
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 //iNES header constants
-#define MIN_NES_HEADER_LEN		8			//min nes header valid byte len
+#define INES_HEADER_LEN		    8			//min nes header valid byte len
 #define SUB_CHAR				26			//used as escape sequence in MS-DOS
 
 typedef char					int8;
@@ -15,8 +15,26 @@ typedef unsigned short			uint16;
 typedef unsigned int			uint32;
 typedef unsigned long long		uint64;
 
+#define PRG_LEN 16384   //16kb
+#define CHR_LEN 8192    //8kb
+
 int16 num_PRGs;
 int16 num_CHRs;
+
+enum Instructions {
+    ADC, AND, ASL, BCC, BCS, BEQ, BIT, BMI, BNE, BPL, BRK, BVC, BVS, CLC, 
+    CLD, CLI, CLV, CMP, CPX, CPY, DEC, DEX, DEY, EOR, INC, INX, INY, JMP, 
+    JSR, LDA, LDX, LDY, LSR, NOP, ORA, PHA, PHP, PLA, PLP, ROL, ROR, RTI, 
+    RTS, SBC, SEC, SED, SEI, STA, STX, STY, TAX, TAY, TSX, TXA, TXS, TYA
+};
+
+struct OpCode {
+
+    int8* name;
+};
+
+OpCode op_codes = 
+{ "BRK" };
 
 int main(int argc, char* argv[]) {
 	std::ifstream file;
@@ -24,7 +42,7 @@ int main(int argc, char* argv[]) {
 	int32 size = file.tellg();
 	file.seekg(0);
 
-	if (size >= MIN_NES_HEADER_LEN) {
+	if (size >= INES_HEADER_LEN) {
         std::string str(size, ' ');
         file.read(&str[0], size);
 
@@ -34,8 +52,10 @@ int main(int argc, char* argv[]) {
             int8 flag6 = str[6];
             bool hmirror = ~flag6 & 1;
             bool SRAM_enabled = flag6 & 2;
-            bool has_trainer = flag6 & 4;
+            bool has_bp_PRG_RAM = flag6 & 4; //if true, cartridge has 4kb RAM at PPU $2000-$2FFF
             bool VRAM_4screen = flag6 & 8;
+
+            std::string prg_data = str.substr(INES_HEADER_LEN, str.length());
 
             int a = 5;
 		}
