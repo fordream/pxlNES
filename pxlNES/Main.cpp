7249c7cefc4a5d63edbefc2db78dc026e99e2e51
Richman Stewart <flynnspixel@gmail.com>
2015-05-03 02:18:34 +1000

    iNES byte 6 read for options

--- pxlNES/Main.cpp
+++ pxlNES/Main.cpp
@@ -4,7 +4,7 @@
 
 //iNES header constants
 #define MIN_NES_HEADER_LEN		8			//min nes header valid byte len
-#define SUB_CHAR				26			//used as escape sequence
+#define SUB_CHAR				26			//used as escape sequence in MS-DOS
 
 typedef char					int8;
 typedef short					int16;
@@ -15,6 +15,9 @@ typedef unsigned short			uint16;
 typedef unsigned int			uint32;
 typedef unsigned long long		uint64;
 
+int16 num_PRGs;
+int16 num_CHRs;
+
 int main(int argc, char* argv[]) {
 	std::ifstream file;
 	file.open("smb.nes", std::ios::ate | std::ios::binary);
@@ -22,13 +25,19 @@ int main(int argc, char* argv[]) {
 	file.seekg(0);
 
 	if (size >= MIN_NES_HEADER_LEN) {
-		int8* buffer = new int8[size];
-		file.read(buffer, size);
-		std::string str = buffer;
+        std::string str(size, ' ');
+        file.read(&str[0], size);
 
 		if (str[0] == 'N' && str[1] == 'E' && str[2] == 'S' && str[3] == SUB_CHAR) {
-			int num_PRG = str[4];
-			int num_CHR = str[5];
+            num_PRGs = str[4];
+            num_CHRs = str[5];
+            int8 flag6 = str[6];
+            bool hmirror = ~flag6 & 1;
+            bool SRAM_enabled = flag6 & 2;
+            bool has_trainer = flag6 & 4;
+            bool VRAM_4screen = flag6 & 8;
+
+            int a = 5;
 		}
 	}
 
