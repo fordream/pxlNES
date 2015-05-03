992f2682e871bf67d2d467756f26242a58395e82
Richman Stewart <flynnspixel@gmail.com>
2015-05-03 16:18:38 +1000

    Process instruction function that offsets PC depending on addressing mode

--- pxlNES/Main.cpp
+++ pxlNES/Main.cpp
@@ -21,6 +21,13 @@ typedef unsigned long long      uint64;
 int16 num_PRGs;
 int16 num_CHRs;
 
+uint8 a = 0;
+uint8 x = 0;
+uint8 y = 0;
+uint16 pc = 0x8000;
+uint8 sp = 0xfd;
+uint8 p = 0x24;
+
 enum Instruction {
     ADC, AND, ASL, BCC, BCS, BEQ, BIT, BMI, BNE, BPL, BRK, BVC, BVS, CLC,
     CLD, CLI, CLV, CMP, CPX, CPY, DEC, DEX, DEY, EOR, INC, INX, INY, JMP,
@@ -36,7 +43,6 @@ const int8* instruction_names[] = {
 };
 
 enum AddressMode {
-    IMPLIED,
     IMPLICIT,
     ACCUMULATOR,
     IMMEDIATE,
@@ -53,7 +59,7 @@ enum AddressMode {
 };
 
 const int8* add_mode_names[] = {
-    "IMPLIED", "IMPLICIT", "ACCUMULATOR", "IMMEDIATE", "ZERO_PAGE", "ZERO_PAGE_X", "ZERO_PAGE_Y", "RELATIVE",
+    "IMPLICIT", "ACCUMULATOR", "IMMEDIATE", "ZERO_PAGE", "ZERO_PAGE_X", "ZERO_PAGE_Y", "RELATIVE",
     "ABSOLUTE", "ABSOLUTE_X", "ABSOLUTE_Y", "INDIRECT", "INDIRECT_X", "INDIRECT_Y"
 };
 
@@ -61,8 +67,8 @@ struct OpCode {
 
     int8 instruct = UNDEFINED;
     const int8* instruct_name = "UNDEFINED";
-    int8 add_mode = IMPLIED;
-    const int8* add_mode_name = "IMPLIED";
+    int8 add_mode = IMPLICIT;
+    const int8* add_mode_name = "IMPLICIT";
     int8 bytes;
     int8 cycles;
 };
@@ -70,45 +76,154 @@ struct OpCode {
 OpCode* op_list = new OpCode[256];
 
 const int32 pre_op_list[] = {
-    0, BRK, IMPLIED,            1, ORA, INDIRECT_X,             5, ORA, ZERO_PAGE,              6, ASL, ZERO_PAGE,
-    8, PHP, IMPLIED,            9, ORA, IMMEDIATE,              10, ASL, ACCUMULATOR,           13, ORA, ABSOLUTE,
-    14, ASL, ABSOLUTE,          16, BPL, IMPLIED,               17, ORA, INDIRECT_Y,            21, ORA, ZERO_PAGE_X, 
-    22, ASL, ZERO_PAGE_X,       24, CLC, IMPLIED,               25, ORA, ABSOLUTE_Y,            29, ORA, ABSOLUTE_X, 
-    30, ASL, ABSOLUTE_X,        32, JSR, IMPLIED,               33, AND, INDIRECT_X,            36, BIT, ZERO_PAGE, 
-    37, AND, ZERO_PAGE,         38, ROL, ZERO_PAGE,             40, PLP, IMPLIED,               41, AND, IMMEDIATE, 
+    0, BRK, IMPLICIT,           1, ORA, INDIRECT_X,             5, ORA, ZERO_PAGE,              6, ASL, ZERO_PAGE,
+    8, PHP, IMPLICIT,           9, ORA, IMMEDIATE,              10, ASL, ACCUMULATOR,           13, ORA, ABSOLUTE,
+    14, ASL, ABSOLUTE,          16, BPL, IMPLICIT,              17, ORA, INDIRECT_Y,            21, ORA, ZERO_PAGE_X, 
+    22, ASL, ZERO_PAGE_X,       24, CLC, IMPLICIT,              25, ORA, ABSOLUTE_Y,            29, ORA, ABSOLUTE_X, 
+    30, ASL, ABSOLUTE_X,        32, JSR, IMPLICIT,              33, AND, INDIRECT_X,            36, BIT, ZERO_PAGE, 
+    37, AND, ZERO_PAGE,         38, ROL, ZERO_PAGE,             40, PLP, IMPLICIT,              41, AND, IMMEDIATE, 
     42, ROL, ACCUMULATOR,       44, BIT, ABSOLUTE,              45, AND, ABSOLUTE,              46, ROL, ABSOLUTE, 
-    48, BMI, IMPLIED,           49, AND, INDIRECT_Y,            53, AND, ZERO_PAGE_X,           54, ROL, ZERO_PAGE_X, 
-    56, SEC, IMPLIED,           57, AND, ABSOLUTE_Y,            61, AND, ABSOLUTE_X,            62, ROL, ABSOLUTE_X, 
-    64, RTI, IMPLIED,           65, EOR, INDIRECT_X,            69, EOR, ZERO_PAGE,             70, LSR, ZERO_PAGE, 
-    72, PHA, IMPLIED,           73, EOR, IMMEDIATE,             74, LSR, ACCUMULATOR,           76, JMP, ABSOLUTE, 
-    77, EOR, ABSOLUTE,          78, LSR, ABSOLUTE,              80, BVC, IMPLIED,               81, EOR, INDIRECT_Y, 
-    85, EOR, ZERO_PAGE_X,       86, LSR, ZERO_PAGE_X,           88, CLI, IMPLIED,               89, EOR, ABSOLUTE_Y, 
-    93, EOR, ABSOLUTE_X,        94, LSR, ABSOLUTE_X,            96, RTS, IMPLIED,               97, ADC, INDIRECT_X, 
-    101, ADC, ZERO_PAGE,        102, ROR, ZERO_PAGE,            104, PLA, IMPLIED,              105, ADC, IMMEDIATE, 
+    48, BMI, IMPLICIT,          49, AND, INDIRECT_Y,            53, AND, ZERO_PAGE_X,           54, ROL, ZERO_PAGE_X, 
+    56, SEC, IMPLICIT,          57, AND, ABSOLUTE_Y,            61, AND, ABSOLUTE_X,            62, ROL, ABSOLUTE_X, 
+    64, RTI, IMPLICIT,          65, EOR, INDIRECT_X,            69, EOR, ZERO_PAGE,             70, LSR, ZERO_PAGE, 
+    72, PHA, IMPLICIT,          73, EOR, IMMEDIATE,             74, LSR, ACCUMULATOR,           76, JMP, ABSOLUTE, 
+    77, EOR, ABSOLUTE,          78, LSR, ABSOLUTE,              80, BVC, IMPLICIT,              81, EOR, INDIRECT_Y, 
+    85, EOR, ZERO_PAGE_X,       86, LSR, ZERO_PAGE_X,           88, CLI, IMPLICIT,              89, EOR, ABSOLUTE_Y, 
+    93, EOR, ABSOLUTE_X,        94, LSR, ABSOLUTE_X,            96, RTS, IMPLICIT,              97, ADC, INDIRECT_X, 
+    101, ADC, ZERO_PAGE,        102, ROR, ZERO_PAGE,            104, PLA, IMPLICIT,             105, ADC, IMMEDIATE, 
     106, ROR, ACCUMULATOR,      108, JMP, INDIRECT,             109, ADC, ABSOLUTE,             110, ROR, ABSOLUTE, 
-    112, BVS, IMPLIED,          113, ADC, INDIRECT_Y,           117, ADC, ZERO_PAGE_X,          118, ROR, ZERO_PAGE_X, 
-    120, SEI, IMPLIED,          121, ADC, ABSOLUTE_Y,           125, ADC, ABSOLUTE_X,           126, ROR, ABSOLUTE_X, 
+    112, BVS, IMPLICIT,         113, ADC, INDIRECT_Y,           117, ADC, ZERO_PAGE_X,          118, ROR, ZERO_PAGE_X, 
+    120, SEI, IMPLICIT,         121, ADC, ABSOLUTE_Y,           125, ADC, ABSOLUTE_X,           126, ROR, ABSOLUTE_X, 
     129, STA, INDIRECT_X,       132, STY, ZERO_PAGE,            133, STA, ZERO_PAGE,            134, STX, ZERO_PAGE, 
-    136, DEY, IMPLIED,          138, TXA, IMPLIED,              140, STY, ABSOLUTE,             141, STA, ABSOLUTE, 
-    142, STX, ABSOLUTE,         144, BCC, IMPLIED,              145, STA, INDIRECT_Y,           148, STY, ZERO_PAGE_X, 
-    149, STA, ZERO_PAGE_X,      150, STX, ZERO_PAGE_Y,          152, TYA, IMPLIED,              153, STA, ABSOLUTE_Y, 
-    154, TXS, IMPLIED,          157, STA, ABSOLUTE_X,           160, LDY, IMMEDIATE,            161, LDA, INDIRECT_X, 
+    136, DEY, IMPLICIT,         138, TXA, IMPLICIT,             140, STY, ABSOLUTE,             141, STA, ABSOLUTE, 
+    142, STX, ABSOLUTE,         144, BCC, IMPLICIT,             145, STA, INDIRECT_Y,           148, STY, ZERO_PAGE_X, 
+    149, STA, ZERO_PAGE_X,      150, STX, ZERO_PAGE_Y,          152, TYA, IMPLICIT,             153, STA, ABSOLUTE_Y, 
+    154, TXS, IMPLICIT,         157, STA, ABSOLUTE_X,           160, LDY, IMMEDIATE,            161, LDA, INDIRECT_X, 
     162, LDX, IMMEDIATE,        164, LDY, ZERO_PAGE,            165, LDA, ZERO_PAGE,            166, LDX, ZERO_PAGE, 
-    168, TAY, IMPLIED,          169, LDA, IMMEDIATE,            170, TAX, IMPLIED,              172, LDY, ABSOLUTE, 
-    173, LDA, ABSOLUTE,         174, LDX, ABSOLUTE,             176, BCS, IMPLIED,              177, LDA, INDIRECT_Y, 
-    180, LDY, ZERO_PAGE_X,      181, LDA, ZERO_PAGE_X,          182, LDX, ZERO_PAGE_Y,          184, CLV, IMPLIED, 
-    185, LDA, ABSOLUTE_Y,       186, TSX, IMPLIED,              188, LDY, ABSOLUTE_X,           189, LDA, ABSOLUTE_X, 
+    168, TAY, IMPLICIT,         169, LDA, IMMEDIATE,            170, TAX, IMPLICIT,             172, LDY, ABSOLUTE, 
+    173, LDA, ABSOLUTE,         174, LDX, ABSOLUTE,             176, BCS, IMPLICIT,             177, LDA, INDIRECT_Y, 
+    180, LDY, ZERO_PAGE_X,      181, LDA, ZERO_PAGE_X,          182, LDX, ZERO_PAGE_Y,          184, CLV, IMPLICIT, 
+    185, LDA, ABSOLUTE_Y,       186, TSX, IMPLICIT,             188, LDY, ABSOLUTE_X,           189, LDA, ABSOLUTE_X, 
     190, LDX, ABSOLUTE_Y,       192, CPY, IMMEDIATE,            193, CMP, INDIRECT_X,           196, CPY, ZERO_PAGE, 
-    197, CMP, ZERO_PAGE,        198, DEC, ZERO_PAGE,            200, INY, IMPLIED,              201, CMP, IMMEDIATE, 
-    202, DEX, IMPLIED,          204, CPY, ABSOLUTE,             205, CMP, ABSOLUTE,             206, DEC, ABSOLUTE, 
-    208, BNE, IMPLIED,          209, CMP, INDIRECT_Y,           213, CMP, ZERO_PAGE_X,          214, DEC, ZERO_PAGE_X, 
-    216, CLD, IMPLIED,          217, CMP, ABSOLUTE_Y,           221, CMP, ABSOLUTE_X,           222, DEC, ABSOLUTE_X, 
+    197, CMP, ZERO_PAGE,        198, DEC, ZERO_PAGE,            200, INY, IMPLICIT,             201, CMP, IMMEDIATE, 
+    202, DEX, IMPLICIT,         204, CPY, ABSOLUTE,             205, CMP, ABSOLUTE,             206, DEC, ABSOLUTE, 
+    208, BNE, IMPLICIT,         209, CMP, INDIRECT_Y,           213, CMP, ZERO_PAGE_X,          214, DEC, ZERO_PAGE_X, 
+    216, CLD, IMPLICIT,         217, CMP, ABSOLUTE_Y,           221, CMP, ABSOLUTE_X,           222, DEC, ABSOLUTE_X, 
     224, CPX, IMMEDIATE,        225, SBC, INDIRECT_X,           228, CPX, ZERO_PAGE,            229, SBC, ZERO_PAGE, 
-    230, INC, ZERO_PAGE,        232, INX, IMPLIED,              233, SBC, IMMEDIATE,            234, NOP, IMPLIED, 
-    236, CPX, ABSOLUTE,         237, SBC, ABSOLUTE,             238, INC, ABSOLUTE,             240, BEQ, IMPLIED, 
-    241, SBC, INDIRECT_Y,       245, SBC, ZERO_PAGE_X,          246, INC, ZERO_PAGE_X,          248, SED, IMPLIED, 
+    230, INC, ZERO_PAGE,        232, INX, IMPLICIT,             233, SBC, IMMEDIATE,            234, NOP, IMPLICIT, 
+    236, CPX, ABSOLUTE,         237, SBC, ABSOLUTE,             238, INC, ABSOLUTE,             240, BEQ, IMPLICIT, 
+    241, SBC, INDIRECT_Y,       245, SBC, ZERO_PAGE_X,          246, INC, ZERO_PAGE_X,          248, SED, IMPLICIT, 
     249, SBC, ABSOLUTE_Y,       253, SBC, ABSOLUTE_X,           254, INC, ABSOLUTE_X
 };
+
+std::string prg_data;
+
+void process_instruct() {
+    OpCode& op = op_list[(uint8)prg_data[pc - 0x8000]];
+
+    std::cout << op.instruct_name << ", " << op.add_mode_name << "\n";
+
+    ++pc;
+    switch (op.add_mode) {
+        case IMPLICIT:
+            break;
+        case ACCUMULATOR:
+            break;
+        case IMMEDIATE:
+            ++pc;
+            break;
+        case ZERO_PAGE:
+            ++pc;
+            break;
+        case ZERO_PAGE_X:
+            ++pc;
+            break;
+        case ZERO_PAGE_Y:
+            ++pc;
+            break;
+        case RELATIVE:
+            ++pc;
+            break;
+        case ABSOLUTE:
+            pc += 2;
+            break;
+        case ABSOLUTE_X:
+            pc += 2;
+            break;
+        case ABSOLUTE_Y:
+            pc += 2;
+            break;
+        case INDIRECT:
+            pc += 2;
+            break;
+        case INDIRECT_X:
+            pc += 2;
+            break;
+        case INDIRECT_Y:
+            pc += 2;
+            break;
+    }
+
+    switch (op.instruct) {
+        case ADC: break;
+        case AND: break; 
+        case ASL: break;
+        case BCC: break;
+        case BCS: break;
+        case BEQ: break;
+        case BIT: break;
+        case BMI: break;
+        case BNE: break;
+        case BPL: break;
+        case BRK: break;
+        case BVC: break;
+        case BVS: break;
+        case CLC: break;
+        case CLD: break;
+        case CLI: break;
+        case CLV: break;
+        case CMP: break;
+        case CPX: break;
+        case CPY: break;
+        case DEC: break;
+        case DEX: break;
+        case DEY: break;
+        case EOR: break;
+        case INC: break;
+        case INX: break;
+        case INY: break;
+        case JMP: break;
+        case JSR: break;
+        case LDA: break;
+        case LDX: break;
+        case LDY: break;
+        case LSR: break;
+        case NOP: break;
+        case ORA: break;
+        case PHA: break;
+        case PHP: break;
+        case PLA: break;
+        case PLP: break;
+        case ROL: break;
+        case ROR: break;
+        case RTI: break;
+        case RTS: break;
+        case SBC: break;
+        case SEC: break;
+        case SED: break;
+        case SEI: break;
+        case STA: break;
+        case STX: break;
+        case STY: break;
+        case TAX: break;
+        case TAY: break;
+        case TSX: break;
+        case TXA: break;
+        case TXS: break;
+        case TYA: break;
+        default:  break;
+    }
+}
 
 int main(int argc, char* argv[]) {
     for (int n = 0; n < 453; n += 3) {
@@ -137,11 +252,10 @@ int main(int argc, char* argv[]) {
             bool has_bp_PRG_RAM = flag6 & 4; //if true, cartridge has 4kb RAM at PPU $2000-$2FFF
             bool VRAM_4screen = flag6 & 8;
 
-            std::string prg_data = str.substr(INES_HEADER_LEN, str.length());
+            prg_data = str.substr(INES_HEADER_LEN, str.length());
 
             for (int n = 0; n < prg_data.length(); ++n) {
-                uint8 op = prg_data[n];
-                std::cout << op_list[op].instruct_name << ", " << op_list[op].add_mode_name << "\n";
+                process_instruct();
             }
         }
     }
